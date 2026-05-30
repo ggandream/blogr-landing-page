c2755fd21a4a6f0e7f13836a729606bac73d10eb
Andrea <ggandream24@gmail.com>
2026-05-29 23:11:10 -0600

    feature:mobile version

---
+++ app.js
@@ -0,0 +1,34 @@
+const $nav__items = document.querySelectorAll(".nav__item");
+const $header__menu = document.querySelector(".header__menu");
+const $dropdown = document.querySelector(".dropdown");
+
+$nav__items.forEach(function (nav__item) {
+  nav__item.addEventListener("click", function () {
+    nav__item.nextElementSibling.classList.toggle("hidden");
+    nav__item.firstElementChild.classList.toggle("upsidedown");
+
+    const isExpanded = nav__item.getAttribute("aria-expanded") === "true";
+    nav__item.setAttribute("aria-expanded", !isExpanded);
+  });
+});
+
+$header__menu.addEventListener("click", function () {
+  $dropdown.classList.toggle("hidden");
+
+  if (
+    $header__menu.firstElementChild.getAttribute("src") ===
+    "./images/icon-hamburger.svg"
+  ) {
+    $header__menu.firstElementChild.setAttribute(
+      "src",
+      "./images/icon-close.svg",
+    );
+    $dropdown.setAttribute("aria-hidden", false);
+  } else {
+    $header__menu.firstElementChild.setAttribute(
+      "src",
+      "./images/icon-hamburger.svg",
+    );
+    $dropdown.setAttribute("aria-hidden", true);
+  }
+});
